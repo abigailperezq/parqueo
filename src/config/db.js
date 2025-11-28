8926fc93677b813a2237c57310cf710ab748259f
Jackeline Pérez <jackelinperezq@gmail.com>
2025-11-28 16:42:11 -0600

    cambiando a aiven mysql

--- src/config/db.js
+++ src/config/db.js
@@ -2,10 +2,10 @@ import mysql from 'mysql2/promise';
 import fs from 'fs';
 
 const pool = mysql.createPool({
-    host: 'gateway01.us-east-1.prod.aws.tidbcloud.com',    
-    port: 4000,
-    user: 'eqhyonJX17XWPcv.root',            
-    password: 'OumYkHg6V6fUFpM9', 
+    host: 'mysql-tpi-parqueotpi.g.aivencloud.com',    
+    port: 27148,
+    user: 'avnadmin',            
+    password: 'AVNS_r4wLPKYVYCKW8TKW8JN', 
     
     database: 'parqueo',  
  
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import app from "./app.js";
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 27148;
 
 app.listen(PORT, () => {
     console.log(`Servidor funcionando en el puerto ${PORT}`);
